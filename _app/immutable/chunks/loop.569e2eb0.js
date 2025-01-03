48cbd1e700138c41deba80d4bf40abcbcd0446e2
JustinTamrin <JustinTamrin@users.noreply.github.com>
2025-01-03 10:17:42 +0000

    deploy: bf88cbda83a49aae3e74965467ef9f994ca9ce80

---
+++ _app/immutable/chunks/loop.569e2eb0.js
@@ -0,0 +1,1 @@
+import{w as i}from"./writeAndUnwriteText.ef79969e.js";import{a as n,m as r}from"./animationSetup.f385820c.js";const p=async(t,e)=>{const{options:o,elements:a}=n(t,e);for(;;){r(t);for(const s of a)await i(s,o)}return{update(){},destroy(){}}};export{p as default};
